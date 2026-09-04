bec77ef9f97d57fcc39dd1b65bc57d63189bcfc5
Steven Xu <91140371+LaughingPenguin@users.noreply.github.com>
2026-09-04 13:39:15 -0400

    Add catppuccin theme (#2)
    * feat: add catppuccin theme
    
    * feat: update init lua

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -3,17 +3,17 @@ vim.g.maplocalleader = "\\"
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-  if vim.v.shell_error ~= 0 then
-    vim.api.nvim_echo({
-      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
-      { "\nPress any key to exit..." },
-    }, true, {})
-    vim.fn.getchar()
-    os.exit(1)
-  end
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+	if vim.v.shell_error ~= 0 then
+		vim.api.nvim_echo({
+			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+			{ out, "WarningMsg" },
+			{ "\nPress any key to exit..." },
+		}, true, {})
+		vim.fn.getchar()
+		os.exit(1)
+	end
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -21,24 +21,19 @@ require("config.options")
 require("config.keymaps")
 
 require("lazy").setup({
-  spec = {
-    { import = "plugins" },
-  },
-  checker = { enabled = true },
+	spec = {
+		{ import = "plugins" },
+	},
+	checker = { enabled = true },
 })
-
-vim.cmd.colorscheme("catppuccin")
-vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-vim.api.nvim_set_hl(0, "CursorLine", { bg = "none" })
-vim.api.nvim_set_hl(0, "ColorColumn", { bg = "none" })
-vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
-vim.api.nvim_set_hl(0, "LineNr", { bg = "none" })
 
 vim.api.nvim_create_autocmd({ "FocusGained", "BufEnter", "CursorHold", "CursorHoldI" }, {
-  pattern = "*",
-  callback = function()
-    if vim.fn.mode() ~= 'c' then
-      vim.cmd("checktime")
-    end
-  end,
+	pattern = "*",
+	callback = function()
+		if vim.fn.mode() ~= 'c' then
+			vim.cmd("checktime")
+		end
+	end,
 })
+
+vim.cmd.colorscheme("catppuccin-nvim")
