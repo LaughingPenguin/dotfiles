523e74a416eef31fed0cbbb53c37336fdacc183f
Steven Xu <91140371+LaughingPenguin@users.noreply.github.com>
2026-09-07 03:36:39 -0400

    Improve code editor UI (#3)

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -1,12 +1,21 @@
 return {
-  "nvim-lualine/lualine.nvim",
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  config = function()
-    require("lualine").setup({
-      options = {
-        theme = "auto", 
-        icons_enabled = true,
-      },
-    })
-  end,
+	"nvim-lualine/lualine.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	config = function()
+		require("lualine").setup({
+			options = {
+				theme = "auto", 
+				icons_enabled = true,
+			},
+			extensions = { "nvim-tree" },
+			sections = {
+				lualine_c = {
+					{
+						"filename",
+						path = 1,
+					},
+				},
+			},
+		})
+	end,
 }
