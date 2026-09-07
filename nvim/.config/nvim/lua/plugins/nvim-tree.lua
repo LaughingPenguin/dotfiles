3d247ea101a1075765493dc62c1723a7d8891679
Haoran Xu <sxu2469@gmail.com>
2026-09-07 03:35:12 -0400

    feat: declutter and improve ui info

--- nvim/.config/nvim/lua/plugins/nvim-tree.lua
+++ nvim/.config/nvim/lua/plugins/nvim-tree.lua
@@ -3,6 +3,18 @@ return {
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	config = function()
 		require("nvim-tree").setup({
+			view = { width = 30 },
+			renderer = {
+				group_empty = true,
+				-- indent_markers = { enable = true },
+				indent_width = 2,
+				root_folder_modifier = ":t",
+				icons = {
+					show = {
+						folder_arrow = false,
+					},
+				},
+			},
 			actions = {
 				remove_file = {
 					close_window = false,
